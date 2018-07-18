0c47f71fbbd0738fa5dd18b0cfd7e6c36dc8a7b4
Lisbel Cruz <Lcruztaveras@gmail.com>
2018-07-18 11:35:22 -0500

    adding not found 404 page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,16 +1,63 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HeroBackground from '../components/HeroBackground';
+import ReactGA from 'react-ga';
 
-const NotFoundPage = () => (
+class NotFoundPage extends React.Component {
 
-        <div>
 
-            <h2>404 - Page Not Found</h2>
-            <Link to="/"> Home </Link>
+    handleScrollToTop = () => {
 
-        </div>
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
         
-)
+        // Tracker event
+        ReactGA.event({
+            category: 'Navigation',
+            action: 'Back Home ( from page not found)',
+        });
+
+        // changing the page
+        const page = '/';  
+        this.props.history.push(page);
+
+    }
+
+    render(){
+
+        return(
+
+            <div className="notFound">
+                <div className="wrap container-fluid">      
+                        <div className="row center-xs project__row_container">
+                            <div className="col-xs-9 col-md-6">
+                                <div className="box project__header">
+
+                                    <h1 className="header_section">$#!@, 404!</h1>
+
+                                    <p>Houston, we have a problem!</p>
+                                    <p>We cannot find what you're looking for.</p>
+
+                                     <div className="btn btn--round" onClick={this.handleScrollToTop}> 
+                                        <Link to="/"> Back Home </Link>
+                                    </div>
+
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+
+                <HeroBackground />
+
+            </div>
+        )
+    
+    }
+        
+}
 
 export default NotFoundPage
 
